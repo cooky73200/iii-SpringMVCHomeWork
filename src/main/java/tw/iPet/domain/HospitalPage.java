4d0de2fb858b2032cd050d430e2154308696d455
OKder <cooky73200@gmail.com>
2021-09-10 01:32:09 +0800

    軒達好棒棒

--- src/main/java/tw/iPet/domain/HospitalPage.java
+++ src/main/java/tw/iPet/domain/HospitalPage.java
@@ -9,15 +9,51 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @Builder @NoArgsConstructor @AllArgsConstructor
 @Component
 public class HospitalPage {
 
-	@Builder.Default
+
 	private int pageCount = 10;
 	private int page;
 	private int totalCount;
 	private List<Hospital> hospitalList;
+
+
+
+
+
+	public HospitalPage() {
+		super();
+	}
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	public List<Hospital> getHospitalList() {
+		return hospitalList;
+	}
+	public void setHospitalList(List<Hospital> hospitalList) {
+		this.hospitalList = hospitalList;
+	}
+
+
+
+
+
 
 
 }
